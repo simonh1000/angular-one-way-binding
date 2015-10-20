547d6d37d34fa030002050a6b2e2b29d1a372f8c
Simon Hampton <hotbelgo@gmail.com>
2015-10-20 09:22:12 +0200

    rewrite of counters

--- src/app/components/counters/counters-component.ts
+++ src/app/components/counters/counters-component.ts
@@ -1,7 +1,9 @@
 import { Component, View } from 'angular2/angular2';
 import { CounterComponent } from '../counter/counter-component';
 
-@Component({ selector: 'counters' })
+@Component({ 
+  selector: 'counters'
+})
 @View({
   templateUrl: 'app/components/counters/counters.html',
   directives: [CounterComponent]
@@ -16,18 +18,8 @@ export class CountersComponent {
     }
   }
   
-  mchangetop(delta) {
-    this.mchange('top')(delta);  
+  pupdate(modelName) {
+    return ( newVal : number => this.model[modelName] = newVal);
   }
   
-  mchange(countName){
-    return (delta => {
-      console.log(countName, delta);      
-      this.model[countName] += delta);
-    });
-  }
-  
-  eventHandler(evt) {
-    console.log(evt);    
-  }
 };
